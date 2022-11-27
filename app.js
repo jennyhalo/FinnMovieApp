8fd3f9217ed72c5c1ee1b2057a5edec00c6ee4aa
jennyhalo <jenny.halonen@student.laurea.fi>
2022-11-27 22:27:19 +0200

    second commit

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ window.addEventListener('load', ()=> {
 })
 
 valikko.addEventListener('change', ()=> {
+    document.querySelectorAll('li').forEach(item => {
+        item.remove();
+        });
     const arvo = document.getElementById('valikko').value;
     const areaId = arvo.substring(0,4);
     console.log(areaId);
@@ -32,29 +35,64 @@ valikko.addEventListener('change', ()=> {
         console.log(xml);
         let event = xml.getElementsByTagName('Event');
         for (let i = 0; i < event.length; i++) {
+            let elokuvanKansi = xml.getElementsByTagName('EventLargeImagePortrait')[i].childNodes[0].nodeValue;
             let elokuvanNimi = xml.getElementsByTagName('OriginalTitle')[i].childNodes[0].nodeValue;
             let tuotantoVuosi = xml.getElementsByTagName('ProductionYear')[i].childNodes[0].nodeValue;
-            let ikäsuositus = xml.getElementsByTagName('Rating')[i].childNodes[0].nodeValue;
+            let ikäsuositus = xml.getElementsByTagName('RatingImageUrl')[i].childNodes[0].nodeValue;
             let genret = xml.getElementsByTagName('Genres')[i].childNodes[0].nodeValue;
-            let lyhytKuvaus = xml.getElementsByTagName('ShortSynopsis')[i].childNodes[0].nodeValue;
+            let Kuvaus = xml.getElementsByTagName('Synopsis')[i].childNodes[0].nodeValue;
+            console.log(elokuvanKansi);
             console.log(elokuvanNimi);
             const elokuvaLista = document.getElementById('myUl');
+            let ratingLabel = document.createElement('img');
+            let movieImg = document.createElement('img');
             let elokuva = document.createElement('li');
             let br1 = document.createElement('br');
             let br2 = document.createElement('br');
             let br3 = document.createElement('br');
             let br4 = document.createElement('br');
             let br5 = document.createElement('br');
+            let movieNameDiv = document.createElement('div');
+            let movieYearDiv = document.createElement('div');
+            let movieAgeLimitDiv = document.createElement('div');
+            let movieGenreDiv = document.createElement('div');
+            let movieDescriptionDiv = document.createElement('div');
 
-            elokuva.append(elokuvanNimi);
+            elokuva.setAttribute('class', 'container d-flex mt-5 bg-dark');
+            elokuva.setAttribute('id', 'elokuvanmuotoilu');
+            ratingLabel.setAttribute('src', ikäsuositus);
+            movieImg.setAttribute('src', elokuvanKansi);
+            movieImg.setAttribute('class', 'movieImg');
+            movieImg.setAttribute('width', '200px');
+            movieImg.setAttribute('height', '296px');
+            movieNameDiv.setAttribute('class', 'movieNameDiv');
+            movieYearDiv.setAttribute('class', 'movieYearDiv');
+            movieAgeLimitDiv.setAttribute('class', 'movieAgeLimitDiv');
+            movieGenreDiv.setAttribute('class', 'movieGenreDiv');
+            movieDescriptionDiv.setAttribute('class', 'movieDescriptionDiv');
+
+            elokuva.append(movieImg);
+            movieNameDiv.append(elokuvanNimi);
+            elokuva.append(movieNameDiv);
             elokuva.append(br1);
-            elokuva.append(tuotantoVuosi);
+            movieYearDiv.append(tuotantoVuosi);
+            movieNameDiv.append(movieYearDiv);
             elokuva.append(br2);
-            elokuva.append(ikäsuositus);
+            
+            var ikäsuosituksentarkistus = /rating_large_S\.png|rating_large_18\.png|rating_large_7\.png|rating_large_12\.png|rating_large_16\.png/.test(ikäsuositus);
+            if (ikäsuosituksentarkistus === true) {
+            movieAgeLimitDiv.append(ratingLabel);
+            movieNameDiv.append(movieAgeLimitDiv);
+            }
+            
+            
+            
             elokuva.append(br3);
-            elokuva.append(genret);
+            movieGenreDiv.append(genret);
+            elokuva.append(movieGenreDiv);
             elokuva.append(br4);
-            elokuva.append(lyhytKuvaus);
+            movieDescriptionDiv.append(Kuvaus);
+            movieGenreDiv.append(movieDescriptionDiv);
             elokuva.append(br5);
             elokuvaLista.appendChild(elokuva);
 
@@ -62,4 +100,18 @@ valikko.addEventListener('change', ()=> {
         }
     });
         
+})
+
+const myBtn = document.getElementById('myBtn');
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myBtn.style.display = "block";
+  } else {
+    myBtn.style.display = "none";
+  }
+}
+myBtn.addEventListener('click', ()=> {
+    document.documentElement.scrollTop = 0; 
 })
